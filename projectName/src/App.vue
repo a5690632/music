b378144fee463f4f97e54b3785bff5825bfdd5f8
215200924@qq.com <215200924@qq.com>
2017-07-29 21:33:41 +0800

    music

---
+++ projectName/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+ <vhead></vhead>
+</template>
+
+<script>
+import vhead from './components/head/head'
+
+export default {
+  name: 'app',
+  components: {
+    vhead,
+  }
+}
+</script>
+
+<style>
+@import '/static/font-awesome/css/font-awesome.min.css';
+
+
+
+
+</style>
